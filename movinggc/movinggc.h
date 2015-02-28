f02b5882dd73a1ec38b0a1ec6dd8e71b72159482
Luca Saiu <positron@gnu.org>
2015-02-28 01:59:09 +0100

    movinggc overhaul
    A big whole-night overhaul of my semispace exact-pointer-finding
    garbage collector including fixes, optimizations, cosmetic changes and
    algorithmic improvements.  The collector is not yet integrated into
    epsilon, but it will serve as a reasonable C benchmark against which
    to compare the future native implementation in epsilon.
    
    This is not the parallel collector.  It's a completely different
    design, sequential and comparatively simple but very fast,
    particularly with high allocation rate.  It can be adapted into the
    young generation of a generational and/or parallel GC.
    
    It might actually make sense to package this collector separately from
    epsilon, as other people might find it useful.
    
    Exact pointer finding is very annoying to be used from C, but the
    interface should be suitable for compilers and interpreters, thanks to
    the idea of asynchronous root scanning.  Write me if you are
    interested.
    
    * movinggc/movinggc.c, movinggc/movinggc.h, movinggc/tags.h,
    * movinggc/features.h, movinggc/driver.c: Here
    
    * movinggc/CLEAN, movinggc/GO, movinggc/README, movinggc/TODO: New.

--- movinggc/movinggc.h
+++ movinggc/movinggc.h
@@ -1,6 +1,7 @@
 /* This file is part of GNU epsilon.
 
    Copyright (C) 2012 Université Paris 13
+   Copyright (C) 2015 Luca Saiu
    Written by Luca Saiu
 
    GNU epsilon is free software: you can redistribute it and/or modify
@@ -20,33 +21,61 @@
 #ifndef MOVINGGC_H_
 #define MOVINGGC_H_
 
-#include "tags.h"
+/* It's important to include features.h because of global register
+   variables, which have to be visible in all compilation units. */
 #include "features.h"
+#include "tags.h"
 
 /* Initialize and finalize: */
-void movinggc_initialize(void);
+void movinggc_initialize (void);
 
-/* Register an array of roots: */
-void register_root(void **pointer_to_roots,
-                   size_t size_in_words);
+/* Register an array of permanent roots: */
+void register_roots (void **pointer_to_roots, size_t size_in_words);
 
-/* Handle temporary roots: */
-void movinggc_push_root(void **pointer_to_root);
-void movinggc_pop_root(void);
+/* Handle temporary roots.  It is not allowed to register permanent
+   roots when any temporary root is active.  Attempting to do so will
+   likely have disastrous effects, and is not checked for. */
+void movinggc_push_dynamic_root (void **pointer_to_root);
+void movinggc_pop_dynamic_root (void);
+void movinggc_pop_dynamic_roots (size_t how_many);
 
-/* Allocate: this is the most frequently used function, of course. It
-   returns a NON-tagged pointer. */
-void *movinggc_allocate(const size_t size_in_words);
+/* Pushing and popping temporary roots all the time is very expensive.
+   When possible it is much better to scan for roots asynchronously,
+   right before a collection, for example by examining an execution
+   stack with a known structure.  This is the intended use case for
+   the following hooks, to be run before and after collection: the
+   pre-GC hook should push temporary roots, and the post-GC hook
+   should pop them.  The number of active temporary roots must be the
+   same at pre-GC entry and at post-GC exit time.  Hooks may not
+   allocate from the GC'd heap. */
+typedef void (*movinggc_hook_t) (void *argument);
+void movinggc_set_pre_hook (movinggc_hook_t hook);
+void movinggc_set_post_hook (movinggc_hook_t hook);
+void movinggc_set_hook_argument (void *argument);
 
-/* Explicit GC and tuning: */
-void movinggc_resize_semispaces(const size_t new_semispace_size_in_words);
-void movinggc_gc(void)
-  __attribute__((noinline));
+/* Allocate a new heap object and return an UNtagged pointer to it.
+
+   Before allocation a collection may be triggered.  In that case
+   the allocation function executes, in this order:
+   1) the pre-GC hook, if any, passing the GC hook argument (whose content
+      may be modified by either hook);
+   2) a garbage collection;
+   3) the post-GC hook, if any, passing the same GC hook argument;
+   4) a heap resize, if needed.
+
+   The char version is faster, but it still assumes objects to have a
+   size which is a multiple of the word size. */
+void *movinggc_allocate_chars (const size_t size_in_chars)
+  __attribute__ ((hot, malloc));
+void *movinggc_allocate_words (const size_t size_in_words)
+  __attribute__ ((hot, malloc, flatten));
+
+/* Explicit GC.  Also executes the pre- and post-GC hooks, if any. */
+void movinggc_gc (void) __attribute__ ((noinline, cold));
 
 /* Statistics and debugging: */
-void movinggc_dump_free_space_statistics(void);
-float movinggc_fill_ratio(void);
-const char* movinggc_semispace_name_of(void *untagged_pointer);
-long movinggc_gc_no(void); // how many times did we GC?
-
+float movinggc_fill_ratio (void);
+const char *movinggc_semispace_name_of (const void *untagged_pointer);
+long movinggc_gc_no (void); // how many times did we GC?
+double movinggc_allocated_bytes (void); // how many times bytes did we allocate since the beginning?
 #endif // #ifndef MOVINGGC_H_

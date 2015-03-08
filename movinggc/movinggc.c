57570865326aa1b83289f30e085efa59995f91e9
Luca Saiu <positron@gnu.org>
2015-03-08 11:40:33 +0100

    WIP minor changes

--- movinggc/movinggc.c
+++ movinggc/movinggc.c
@@ -58,13 +58,13 @@ static void **movinggc_fromspace_after_payload_end = NULL;;
 #define MOVINGGC_INITIAL_ROOTS_ALLOCATED_SIZE 64
 
 #define MOVINGGC_GENERATION_0_SEMISPACE_WORD_NO \
-  (100 * 1024L / sizeof(void*))//(1 * 1024L / sizeof(void*)) //(32 * 1024 * 1024L / sizeof (void*))
+  (800 * 1024L / sizeof(void*))//(1 * 1024L / sizeof(void*)) //(32 * 1024 * 1024L / sizeof (void*))
 
 #define MOVINGGC_GENERATION_1_SEMISPACE_WORD_NO \
-  (200 * 1024L / sizeof(void*)) //(32 * 1024 * 1024L / sizeof (void*))
+  (1280 * 1024L / sizeof(void*)) //(32 * 1024 * 1024L / sizeof (void*))
 
 #define MOVINGGC_GENERATION_2_SEMISPACE_WORD_NO \
-  (3000 * 1024L * 1024L / sizeof(void*)) //(32 * 1024 * 1024L / sizeof (void*))
+  (3 * 1024L * 1024L / sizeof(void*)) //(32 * 1024 * 1024L / sizeof (void*))
 
 #define MOVINGGC_INITIAL_ALLOCATED_ROOT_NO  1 // FIXME: increase
 
@@ -430,7 +430,7 @@ movinggc_dump_times (void)
             first = false;
           else
             fprintf (stderr, ", ");
-          fprintf (stderr, "%.01f%% generation %i",
+          fprintf (stderr, "%.1f%% generation %i",
                    g->gc_time / total_gc_time * 100, (int)g->generation_index);
         } // for
     } // if

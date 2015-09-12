eda0443cc0a785e48fc11f2d8b2f5ac5edbbd62d
Luca Saiu <positron@gnu.org>
2015-09-12 23:00:11 +0200

    WIP threaded code: start
    * Makefile.am: Here.
    * runtime/test-threaded.c: New.

---
+++ runtime/test-threaded.c
@@ -0,0 +1,30 @@
+/* A program for testing and developing threaded code.
+
+   Copyright (C) 2015 Luca Saiu
+   Written by Luca Saiu
+
+   This file is part of GNU epsilon.
+
+   GNU epsilon is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   GNU epsilon is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with GNU epsilon.  If not, see <http://www.gnu.org/licenses/>. */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+
+int
+main (void)
+{
+  return EXIT_SUCCESS;
+}
